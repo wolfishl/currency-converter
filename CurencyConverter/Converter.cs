4820a92eb4ede0138665ec32f844f327a2ed27c8
lwolfish <58049823+lwolfish@users.noreply.github.com>
2021-05-27 19:01:44 -0400

    Added Currency Converter class - thank you, Rochel!

--- CurencyConverter/Converter.cs
+++ CurencyConverter/Converter.cs
@@ -6,23 +6,102 @@ using System.Threading.Tasks;
 
 namespace CurencyConverter
 {
-    /*
-     * Converts the rates that the user entered in the GUI to the rates specified
-     * using the rates from Service
-     * 
+    /* Allows the following conversion
+     * GBP/EUR
+     * ILS/EUR
+     * USD/EUR
+     * LBP/EUR
      */
     class Converter
     {
-        /*
-         * Starting and Final currency should be one of the 3 letter codes
-         * 
-         */
-        public double convert(double startingAmount, String startingCurrency, String finalCurrency)
+        public double Convert(double startingAmount, string startingCurrency, string finalCurrency)
         {
-            //TODO write this code
-            double finalAmount = 0;
+            double finalAmount;
+            if (startingCurrency == finalCurrency)
+            {
+                finalAmount = startingAmount;
+            }
+            else
+            {
+                // convert starting currency to euro
+                double euroAmount = startingCurrency.Equals("EUR") ? startingAmount : AnyToEuro(startingAmount, startingCurrency);
+                //convert euro start amount to chosen currency
+                switch (finalCurrency)
+                {
+                    case "GBP":
+                        finalAmount = EuroToGBP(euroAmount);
+                        break;
+                    case "ILS":
+                        finalAmount = EuroToILS(euroAmount);
+                        break;
+                    case "USD":
+                        finalAmount = EuroToUSD(euroAmount);
+                        break;
+                    case "LBP":
+                        finalAmount = EuroToLBP(euroAmount);
+                        break;
+                    case "EUR":
+                        finalAmount = startingAmount;
+                        break;
+                    default:
+                        finalAmount = 0;
+                        break;
+                }
+            }
             return finalAmount;
         }
 
+        private double AnyToEuro(double startingAmount, String startingCurrency)
+        {
+            double euroAmount;
+            double rate;
+            switch (startingCurrency)
+            {
+                case "GBP":
+                    rate = Service.getGBP();
+                    euroAmount = startingAmount / rate;
+                    break;
+                case "ILS":
+                    rate = Service.getILS();
+                    euroAmount = startingAmount / rate;
+                    break;
+                case "USD":
+                    rate = Service.getUSD();
+                    euroAmount = startingAmount / rate;
+                    break;
+                case "LBP":
+                    rate = Service.getLBP();
+                    euroAmount = startingAmount / rate;
+                    break;
+                default:
+                    euroAmount = 0;
+                    break;
+            }
+            return euroAmount;
+        }
+
+        private double EuroToGBP(double amount)
+        {
+            double rate = Service.getGBP();
+            return amount * rate;
+        }
+
+        private double EuroToILS(double amount)
+        {
+            double rate = Service.getILS();
+            return amount * rate;
+        }
+
+        private double EuroToUSD(double amount)
+        {
+            double rate = Service.getUSD();
+            return amount * rate;
+        }
+
+        private double EuroToLBP(double amount)
+        {
+            double rate = Service.getLBP();
+            return amount * rate;
+        }
     }
 }
